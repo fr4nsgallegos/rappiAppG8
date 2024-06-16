5a83d46ca1ac34b05a7c21fb133f92425c22d615
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-16 12:39:50 -0500

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:rappiappg8/pages/home_page.dart';
+import 'package:rappiappg8/pages/login_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
     ),
   );
 }
