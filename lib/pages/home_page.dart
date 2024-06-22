ef20803476ebd5fa5e248da2e24155612bb946be
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-21 19:45:15 -0500

    gnav bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -6,8 +7,32 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          // leading: Container(),
-          automaticallyImplyLeading: false,
+            // leading: Container(),
+            // automaticallyImplyLeading: false,
+            ),
+        bottomNavigationBar: GNav(
+          color: Colors.grey[400],
+          activeColor: Colors.orange,
+          tabActiveBorder: Border.all(color: Colors.red),
+          tabBackgroundColor: Colors.grey.shade100,
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBorderRadius: 16,
+          gap: 8,
+          // padding: EdgeInsets.all(8),
+
+          onTabChange: (index) {
+            print(index);
+          },
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: "Tienda",
+            ),
+            GButton(
+              icon: Icons.shopping_bag_rounded,
+              text: "Carrito",
+            )
+          ],
         ),
       ),
     );
